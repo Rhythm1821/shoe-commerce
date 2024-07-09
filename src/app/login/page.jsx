d5eabb80d1cab0302455f91e84f5cf3d54715e8e
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-09 17:07:50 +0530

    register, login frontend complete

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import LoginForm from "../../components/auth/LoginForm";
+
+ 
+export default function LoginPage() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <LoginForm />
+    </div>
+  )
+}
