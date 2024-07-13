b3be523328cf9f31e285754155a9e0e2ed6bdd8a
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-13 15:41:45 +0530

    Update cart api && add to cart api updates

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -1,7 +1,6 @@
 import { buyerAuth } from "@/middlewares/auth";
-import { Cart, CartItem } from "@/models/Cart.model";
+import { Cart } from "@/models/Cart.model";
 import { Product } from "@/models/Product.model";
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 // api/cart (GET)
@@ -17,40 +16,86 @@ export async function GET(request) {
 }
 
 // api/cart (POST)
+
 export async function POST(request) {
+
+    // check if buyer exists
     const isAuthenticated = await buyerAuth(request);
 
     if (!isAuthenticated) {
         return NextResponse.json({message: "Unauthorized"}, { status: 401 })
     }
 
+    // Get the data
     const body = await request.json();
     const { productId, quantity } = body;
 
+    // check if product exists
     if (!(await Product.findById(productId))) {
         return NextResponse.json({message: "Product not found"}, { status: 404 });        
     }
 
+    // check if cart exists
     const cart = await Cart.findOne({buyer: request.user._id});
+
+    // create new cart if not exists
     if (!cart) {
         const newCart = new Cart({
             buyer: request.user._id,
-            cartItems: [productId],
-            quantity
-        });
+            cartItems: [{product: productId, quantity}],            
+        })
         await newCart.save();
         return NextResponse.json({message: "Cart created successfully"}, { status: 201 });
     }
+    
+    // Add product to cart
+    const existingItemIndex = cart.cartItems.findIndex((item) => item.product.toString() === productId);
+    if (existingItemIndex > -1) {
+        // Product already exists in cart, update quantity
+        cart.cartItems[existingItemIndex].quantity += quantity;
+    } else {
+        // Product does not exist in cart, add new item
+        cart.cartItems.push({product: productId, quantity});
+    }
 
-    const cartItem = await CartItem({
-        cart: cart._id,
-        product: productId,
-        quantity
-    })
-
-    cart.cartItems.push(cartItem);
-    cart.quantity += quantity;
     await cart.save();
     return NextResponse.json({message: "Cart updated successfully"}, { status: 200 });
+
 }
 
+export async function PUT(request) {
+
+    // check if buyer exists
+    const isAuthenticated = await buyerAuth(request);
+
+    if (!isAuthenticated) {
+        return NextResponse.json({message: "Unauthorized"}, { status: 401 })
+    }
+
+    // Get the query params id
+    const { productId, quantity } = await request.json();
+
+    // check if cart exists
+    const cart = await Cart.findOne({buyer: request.user._id});
+    if (!cart) {
+        return NextResponse.json({message: "Cart not found"}, { status: 404 });
+    }
+
+    // Update item from cart
+    const existingItemIndex = cart.cartItems.findIndex((item) => item.product.toString() === productId);
+    if (existingItemIndex > -1) {
+        // Product already exists in cart, update quantity
+        cart.cartItems[existingItemIndex].quantity += quantity
+        
+        if (cart.cartItems[existingItemIndex].quantity <= 0) {
+            cart.cartItems.splice(existingItemIndex, 1);
+        }
+    } else {
+        // Product does not exist in cart
+        return NextResponse.json({message: "Product not found in cart"}, { status: 404 });
+    }
+
+    await cart.save();
+
+    return NextResponse.json({message: "Cart updated successfully"}, { status: 200 });
+}
