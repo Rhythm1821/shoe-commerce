b76bd767faa5f5bc85fe643a1192c371c632f618
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-14 17:14:57 +0530

    Component to list all the product

---
+++ src/components/GetAllProducts.jsx
@@ -0,0 +1,52 @@
+"use client"
+import { getProducts } from "@/utils/api-client";
+import { useEffect, useState } from "react";
+
+export default function GetAllProducts() {
+    const [allProducts, setAllProducts] = useState([]);
+
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const products = await getProducts();
+            setAllProducts(products);
+        };
+        fetchProducts();
+    }, []);
+
+    return (
+        <div className="p-6 bg-slate-950 text-yellow-100 rounded shadow-md text-center">
+            <h2 className="text-2xl items-center">GetAllProducts</h2>
+            <div>
+                {
+                    allProducts.length > 0 ? (
+                        allProducts.map((product, index) => (
+                            <div key={index} className="mb-4">
+                                <h3 className="text-xl">{product.name}</h3>
+                                <p>Category: {product.category}</p>
+                                <p>Brand: {product.brand}</p>
+                                <p>Description: {product.description}</p>
+                                <p>Material: {product.material}</p>
+                                <p>Color: {product.color}</p>
+                                <p>Price: ${product.price}</p>
+                                <p>Sizes: {product.sizes.join(", ")}</p>
+                                {product.shoeImages.length > 0 && (
+                                    <div>
+                                        <h4 className="text-lg">Images:</h4>
+                                        <div className="flex space-x-4">
+                                            {product.shoeImages.map((image, imgIndex) => (
+                                                <img key={imgIndex} src={image} alt={`Shoe Image ${imgIndex}`} className="w-24 h-24 object-cover rounded shadow mx-auto" />
+                                            ))}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        ))
+                    ) : (
+                        <p>No products available</p>
+                    )
+                }
+            </div>
+        </div>
+    );
+}
