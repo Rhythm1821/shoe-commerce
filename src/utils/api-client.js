76ffeccaa8dcf38bfaac0953b4e91472065c8cfc
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-15 18:38:06 +0530

    api client file for communicating with api endpoint

---
+++ src/utils/api-client.js
@@ -0,0 +1,91 @@
+
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000'
+
+export async function apiClient(endpoint, options = {}) {
+    const baseUrl = process.env.API_BASE_URL
+    const url = `${baseUrl}/${endpoint}`
+
+    const response = await fetch(url, options)
+
+    if (!response.ok) {
+        throw new Error(`Request failed with status code ${response.status}`)
+    }
+
+    return response.json()
+}
+
+
+export async function fetchProducts() {
+    try {
+        const res = await fetch(`${API_BASE_URL}/api/market`);
+        const data = await res.json();
+        return data.products
+    } catch (error) {
+        console.log("Failed to fetch products", error);
+    }
+}
+
+export async function postRegisterDetails(data, type) {
+    try {
+        const res = await fetch(`${API_BASE_URL}/api/users/${type}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+
+        return res
+    } catch (error) {
+        console.log("Error while posting register details", error);
+    }
+}
+
+// secured fetching
+export async function fetchCart() {
+    try {
+        const res = await fetch(`${API_BASE_URL}/api/cart`, 
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+        });
+        const data = await res.json();
+        return data.cart
+    } catch (error) {
+        console.log("Failed to fetch cart", error);
+    }
+}
+
+export async function fetchLogout() {
+    try {
+        const res = await fetch(`${API_BASE_URL}/api/users/logout`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+        },
+        );
+        const data = await res.json();
+        return data
+    } catch (error) {
+        console.log("Failed to logout", error);
+    }
+}
+
+export async function fetchInventory(){
+    const res = await fetch(`${API_BASE_URL}/api/inventory`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    })
+
+    const data = await res.json();
+    return data
+}
