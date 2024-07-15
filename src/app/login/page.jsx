0103dcc0120d19cec87ed7160d529f7c672fec69
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-15 18:50:01 +0530

    login route updation

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <LoginForm />
+      <LoginForm type={"buyer"} />
     </div>
   )
 }
