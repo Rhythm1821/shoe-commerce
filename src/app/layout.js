b1eb4cbd7401930131f402e84f9711e2f452fa26
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-16 16:44:45 +0530

    layout update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
-'use client';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { Cookies } from "react-cookie";
-import { usePathname } from "next/navigation";
+import { cookies } from "next/headers";
+import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const pathaname = usePathname();
+
+  const heads = headers()
+ 
+  const pathname = heads.get('next-url')
   const noNavRoutes = ['/login', '/register', '/seller/login', '/seller/register']
 
-  const cookie = new Cookies();
+  const cookie = cookies();
   const accessToken = cookie.get('accessToken')?.value;
   const refreshToken = cookie.get('refreshToken')?.value;
   
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {!noNavRoutes.includes(pathaname) && <Navbar isAuthenticated={isAuthenticated} />}
+        {!noNavRoutes.includes(pathname) && <Navbar isAuthenticated={isAuthenticated} />}
         {children}
       </body>
     </html>
