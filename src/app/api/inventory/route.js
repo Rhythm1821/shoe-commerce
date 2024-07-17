4fca55fb665b6154b0f0a83cce65aa65a16b20dc
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-17 15:37:18 +0530

    Api endpoint updation for adding product to inventory

--- src/app/api/inventory/route.js
+++ src/app/api/inventory/route.js
@@ -33,77 +33,85 @@ export async function GET(request) {
 
 // api/inventory (POST)
 export async function POST(request) {
+    console.log("Reached the inventory endpoint");
     const isAuthenticated = await sellerAuth(request);
 
     if (!isAuthenticated) {
         return NextResponse.json({message: "Unauthorized"}, { status: 401 })
     }
 
-    // check if seller exists
-    const currentSeller = await Seller.findById(request.user._id);
-    if (!currentSeller) {
-        return NextResponse.json({message: "You must be a seller"}, { status: 404 });
-    }
-
-    const FormData = await request.formData();
-    
-    const name = FormData.get('name')
-    const description = FormData.get('description')
-    const brand = FormData.get('brand')
-    const material = FormData.get('material')
-    const color = FormData.get('color')
-    const sizes =  JSON.parse(FormData.get('sizes'))
-    const price = Number(FormData.get('price'))
-    const category = FormData.get('category')
-    const stockQuantity = Number(FormData.get('stockQuantity'))
-
-    const ImageData = FormData.getAll('shoeImages')
-    console.log("ImageData", ImageData);
-
-    const shoeImages = []
-    async function saveImages() {
-        // check if brand dir exists
-        const brandDir = `./public/${brand}`;
-        if (!fs.existsSync(brandDir)) {
-            fs.mkdirSync(brandDir);
+    try {
+        // check if seller exists
+        const currentSeller = await Seller.findById(request.user._id);
+        if (!currentSeller) {
+            return NextResponse.json({message: "You must be a seller"}, { status: 404 });
         }
+        console.log("Checking form data...");
+        const FormData = await request.formData(); // error here
+        console.log("FormData line-50", FormData);
+        
+        const name = FormData.get('name')
+        const description = FormData.get('description')
+        const brand = FormData.get('brand')
+        const material = FormData.get('material')
+        const color = FormData.get('color')
+        const sizes =  JSON.parse(FormData.get('sizes'))
+        const price = Number(FormData.get('price'))
+        const category = FormData.get('category')
+        const stockQuantity = Number(FormData.get('stockQuantity'))
+        const ImageData = Array.from(FormData.getAll('shoeImages'));
     
-        for (const Image of ImageData) {
-            const filePath = `./public/${brand}/${Image.name}`;
-            try {
-                await pump(Image.stream(), fs.createWriteStream(filePath));
-                shoeImages.push(`http://localhost:3000/${Image.name}`);
-                console.log(`File ${Image.name} saved successfully to ${filePath}`);
-            } catch (error) {
-                console.error(`Error saving ${Image.name}:`, error);
+        
+        if (!name || !description || !brand || !material || !color || !sizes || !price || !category || !stockQuantity || ImageData.length<1) {
+            return NextResponse.json({message: "Missing required fields"}, { status: 400 });
+        }
+        
+        console.log("Saving images...");
+        const shoeImages = []
+        async function saveImages() {
+            // check if brand dir exists
+            const brandDir = `./public/${brand}`;
+            if (!fs.existsSync(brandDir)) {
+                fs.mkdirSync(brandDir);
             }
-        }
-        }
+        
+            for (const Image of ImageData) {
+                const filePath = `./public/${brand}/${Image.name}`;
+                try {
+                    await pump(Image.stream(), fs.createWriteStream(filePath));
+                    shoeImages.push(`http://localhost:3000/${Image.name}`);
+                    console.log(`File ${Image.name} saved successfully to ${filePath}`);
+                } catch (error) {
+                    console.error(`Error saving ${Image.name}:`, error);
+                }
+            }
+            }
+        
+        await saveImages();
+        
     
-    await saveImages();
     
-
-    if (!name || !description || !brand || !material || !color || !sizes || !price || !category || !stockQuantity || shoeImages.length<1) {
-        return NextResponse.json({message: "Missing required fields"}, { status: 400 });
+        const newProduct = new Product({
+            name,
+            description,
+            brand,
+            material,
+            color,
+            sizes,
+            price,
+            category,
+            stockQuantity,
+            shoeImages
+        })
+    
+        // Add seller
+        newProduct.seller = request.user._id;
+        await newProduct.save();
+    
+        return NextResponse.json({message: "Product added successfully"}, { status: 201 });
+    } catch (error) {
+        console.log("Error adding product:", error);
+        return NextResponse.json({message: error.message}, { status: 500 });
     }
-
-    const newProduct = new Product({
-        name,
-        description,
-        brand,
-        material,
-        color,
-        sizes,
-        price,
-        category,
-        stockQuantity,
-        shoeImages
-    })
-
-    // Add seller
-    newProduct.seller = request.user._id;
-    await newProduct.save();
-
-    return NextResponse.json({message: "Product added successfully"}, { status: 201 });
 }
 
