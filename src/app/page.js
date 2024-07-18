546520804366326238f64a658a0efc10781fff92
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-18 18:14:45 +0530

    home page ui update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import GetAllProducts from "@/components/GetAllProducts";
+import ProductDetail from "@/components/ProductCard";
 
 
 export default function Home() {
   
   
+  {/* <GetAllProducts /> */}
   return (
     <main>
       <GetAllProducts />
