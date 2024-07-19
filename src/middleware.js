44663aec43cdbd921ab78175f6ae2de08719d147
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-19 16:55:14 +0530

    Separate middleware for buyer and seller

--- src/middleware.js
+++ src/middleware.js
@@ -2,14 +2,29 @@ import { NextResponse } from 'next/server'
  
 export function middleware(request) {
   const path = request.nextUrl.pathname
-  const isPublicPath = path === '/login' || path === '/register' || path === '/verify-email'
-  const accessToken = request.cookies.get('accessToken')?.value
-  
-  if (isPublicPath && accessToken) {
-    return NextResponse.redirect(new URL('/', request.url))
+  const type = request.cookies.get('type')?.value
+
+  // For buyer
+  if (type === 'buyer') {
+    const isPublicPath = path === '/login' || path === '/register' || path === '/verify-email'
+    const accessToken = request.cookies.get('accessToken')?.value
+    
+    if (isPublicPath && accessToken) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+
+  // For seller
+  if (type === 'seller') {
+    const isPublicPath = path === '/seller/login' || path === '/seller/register' || path === '/seller/verify-email'
+    const accessToken = request.cookies.get('accessToken')?.value
+    
+    if (isPublicPath && accessToken) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
   }
 }
  
 export const config = {
-  matcher: ['/login', '/register', '/verify-email']
+  matcher: ['/login', '/register', '/verify-email', '/seller/login', '/seller/register', '/seller/verify-email']
 }
