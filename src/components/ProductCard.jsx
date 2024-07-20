d1ab98cf8c857f2f76b1c5f05a621dfcf0ea7875
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-21 00:35:15 +0530

    Improved ui for Product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -25,14 +25,17 @@ export default function Card({ product }) {
   if (!product) return <div>Loading...</div>;
 
   return (
-      <div className="max-w-sm bg-white rounded-lg shadow-lg overflow-hidden mx-auto p-6 transition-transform transform hover:scale-105 w-full">
+      <div className="max-w-sm bg-zinc-50 rounded-2xl shadow-lg overflow-hidden mx-auto p-6 transition-transform transform hover:scale-105 w-full">
         <Link target="_blank" href={`/product/${product._id}`}>
-          <div className="w-full h-64 overflow-hidden rounded-t-lg">
+          <div className="w-full h-64 overflow-hidden rounded-xl">
             <img src={product.shoeImages} alt={product.name} className="w-full h-full object-cover" />
           </div>
           <div className="p-4">
-        <h1 className="text-2xl font-semibold mb-2 truncate">{product.name}</h1>
-        <p className="text-neutral-900 text-center text-lg mb-4 truncate">{product.brand}</p>
+        <div className="flex">
+          <h1 className="text-xl text-gray-600 text-center mb-2 truncate px-9">{product.name}</h1>
+           || 
+          <p className="text-neutral-900 text-center text-xl mb-4 truncate">{product.brand}</p>
+        </div>
         <div className="flex items-center justify-between mb-4">
           <p className="text-lg text-gray-800 font-bold">Rs. {product.price}</p>
           <Button
@@ -40,12 +43,12 @@ export default function Card({ product }) {
               e.preventDefault();
               handleAddToCart(e, product._id, 1);
             }}
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-200"
+            className="bg-blue-500 text-white py-2 px-4 rounded-xl hover:bg-blue-600 transition-colors duration-200"
           >
             Add to Cart
           </Button>
         </div>
-        <button onClick={(e) => {handleAddToCart(e,product._id, 1)}} className="w-full bg-orange-600 text-white py-2 px-4 rounded hover:bg-orange-700 transition-colors duration-200">
+        <button onClick={(e) => {handleAddToCart(e,product._id, 1)}} className="w-full bg-orange-500 text-white py-2 px-4 rounded-xl hover:bg-orange-600 transition-colors duration-200">
           Buy Now
         </button>
     </div>
