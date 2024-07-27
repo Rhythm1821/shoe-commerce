42fd41eaf2610fd9cb9ff38f45a4fa5d8306ed6d
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-27 19:40:16 +0530

    Carousel for multiple images

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -2,6 +2,14 @@
 import { addToCart, fetchProductDetails } from "@/utils/api-client";
 import { useEffect, useState } from "react";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 export default function ProductPage({ params }) {
   const { id } = params;
   const [product, setProduct] = useState({});
@@ -38,11 +46,44 @@ export default function ProductPage({ params }) {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="lg:grid lg:grid-cols-2 lg:gap-12">
         <div className="lg:flex lg:justify-center">
-          <img
-            src={product.shoeImages[0]}
-            alt={product.name}
-            className="rounded-lg shadow-lg"
-          />
+        <Carousel>
+            <CarouselContent>
+              {product.shoeImages.map((image, index) => (
+                <CarouselItem key={index}>
+                    <img
+                      src={image}
+                      alt={product.name}
+                      className="w-full h-full object-cover"
+                    />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="flex items-center justify-center absolute left-0 top-1/2 transform -translate-y-1/2 text-gray-800 hover:bg-gray-100 hover:text-gray-900 rounded-full shadow-lg h-8 w-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+              </svg>
+            </CarouselPrevious>
+            <CarouselNext className="flex items-center justify-center absolute right-0 top-1/2 transform -translate-y-1/2  text-gray-800 hover:bg-gray-200 hover:text-gray-900 rounded-full shadow-lg h-8 w-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+              </svg>
+            </CarouselNext>
+
+          </Carousel>
         </div>
         <div className="mt-8 lg:mt-0">
           <h1 className="text-3xl font-bold text-gray-900">{product.name}</h1>
