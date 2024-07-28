8f23e85862509ee43bc59509695f571ae1ff0ad8
Rhythm1821 <alamrawat1821@gmail.com>
2024-07-28 17:42:04 +0530

    Carousel for multiple images

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,6 +4,15 @@ import { addToCart } from "@/utils/api-client";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
 export default function Card({ product }) {
 
   if (!product) return <div className="text-center text-gray-600">Loading...</div>;
@@ -25,10 +34,48 @@ export default function Card({ product }) {
 
   return (
       <div className="max-w-sm bg-zinc-50 rounded-2xl shadow-lg overflow-hidden mx-auto p-6 transition-transform transform hover:scale-105 w-full">
-        <Link target="_blank" href={`/product/${product._id}`}>
-          <div className="w-full h-64 overflow-hidden rounded-xl">
-            <img src={product.shoeImages} alt={product.name} className="w-full h-full object-cover" />
-          </div>
+        
+          <Carousel>
+            <CarouselContent>
+              {product.shoeImages.map((image, index) => (
+                <CarouselItem key={index}>
+                  <Link target="_blank" href={`/product/${product._id}`}>
+                    <img
+                      src={image}
+                      alt={product.name}
+                      className="w-full h-full object-cover"
+                    />
+                  </Link>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="flex items-center justify-center absolute left-0 top-1/2 transform -translate-y-1/2 text-gray-800 hover:bg-gray-100 hover:text-gray-900 rounded-full shadow-lg h-8 w-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+              </svg>
+            </CarouselPrevious>
+            <CarouselNext className="flex items-center justify-center absolute right-0 top-1/2 transform -translate-y-1/2  text-gray-800 hover:bg-gray-200 hover:text-gray-900 rounded-full shadow-lg h-8 w-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+              </svg>
+            </CarouselNext>
+
+          </Carousel>
+
           <div className="p-4">
         <div className="flex">
           <h1 className="text-xl text-gray-600 text-center mb-2 truncate px-9">{product.name}</h1>
@@ -51,7 +98,6 @@ export default function Card({ product }) {
           Buy Now
         </button>
     </div>
-        </Link> 
       </div>
   );
 }
